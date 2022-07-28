a7a78891dd4cbbe2d6f6d46f7fbc86c77911aeae
caitkuek <caitkuek@gmail.com>
2022-07-29 01:14:20 +0800

    update

--- codewars/highLowNums.js
+++ codewars/highLowNums.js
@@ -1,8 +1,8 @@
 //* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 function highAndLow(num) {
-    numbers = num.split(" ");
+    let numbers = num.split(" ");
     return `${Math.max(...numbers)} ${Math.min(...numbers)}`
 }
 
-console.log(highAndLow("1 2 3 4"))
+console.log(highAndLow("1 2 3 4 5"))
